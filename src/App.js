9e9bc4a07a81dc3edf1c1059f1bd048a243cc7fc
Hamzeh <hawwash76@gmail.com>
2022-10-19 03:47:54 +0300

    router

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
   return (
     <>
       <Header setIsDark={setIsDark} isDark={isDark} />
-      <BrowserRouter basename={process.env.PUBLIC_URL} >
+      <BrowserRouter basename="/test_repository">
           <Routes>
             <Route
               index
